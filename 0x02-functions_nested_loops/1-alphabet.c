a51bd8aef84da3018e0c6bd0b0fdf7bd7b6ef34e
Fredy ONIANGUE <dieuveilleoniangue@yahoo.com>
2023-06-20 17:29:12 -0700

    Add all correct files for mandatory task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char caracter = 'a';
+
+	while (caracter <= 'z')
+	{
+		_putchar(caracter);
+		c++;
+
+	}
+	_putchar('\n');
+
+}
